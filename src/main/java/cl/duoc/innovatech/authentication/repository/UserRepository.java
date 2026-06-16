1e0d81f12139953d6790ead9ca71cc9c54bcb1dd
Richard Moreano <ri.moreano@duocuc.cl>
2026-06-16 00:26:08 -0400

    feat(auth): inicializar dependencias base e implementar modelo de persistencia JPA Usuario

---
+++ src/main/java/cl/duoc/innovatech/authentication/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package cl.duoc.innovatech.authentication.repository;
+
+import cl.duoc.innovatech.authentication.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
